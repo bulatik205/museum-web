2e91cfcf7834b94996a5003b89daf2c099956939
Булат Муллагалиев <bulatmullagal@gmail.com>
2026-04-10 15:25:22 +0400

    update: added csrf token

--- registration/index.php
+++ registration/index.php
@@ -1,13 +1,19 @@
+<?php
+require '../core/handlers/index.php';
+
+$csrf = new CsrfController();
+if (empty($csrf->getCSRF())) {
+    $csrf->setCSRF($csrf->generateCSRF());
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registartion</title>
     <link rel="stylesheet" href="../source/css/pages/auth/index.css">
 </head>
-
 <body>
     <main class="main">
         <div class="main--body">
@@ -32,6 +38,9 @@
             <button onclick="window.location.href='/login/'">Login</button>
         </div>
     </main>
-</body>
 
+    <script>
+        window.CSRF_TOKEN = '<?= $csrf->getCSRF() ?>';
+    </script>
+</body>
 </html>
