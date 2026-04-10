8ebdc9a8e29a6f9cbe1d86c7b28c68f049a3cae9
Булат Муллагалиев <bulatmullagal@gmail.com>
2026-04-10 17:10:26 +0400

    update: added main validations the request

--- core/handlers/registration/index.php
+++ core/handlers/registration/index.php
@@ -1,1 +1,24 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    echo json_encode([
+        'success' => false,
+        'error' => [
+            'code' => 405,
+            'message' => 'Method Not Allowed'
+        ]
+    ]);
+    exit;
+}
+
+$headers = getallheaders();
+
+if (!isset($headers['csrf_token'])) {
+    echo json_encode([
+        'success' => false,
+        'error' => [
+            'code' => 403,
+            'message' => 'Forbidden'
+        ]
+    ]);
+    exit;
+}
