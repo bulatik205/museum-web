3a38ae8295e660a29ae0063ddeed0ba2e7033ea1
Булат Муллагалиев <bulatmullagal@gmail.com>
2026-04-12 13:30:05 +0400

    update: cleared database.config.php and left the return array

--- core/conf/database.config.php
+++ core/conf/database.config.php
@@ -1,24 +1,8 @@
 <?php
-$main_dsn = "mysql";                # databse: mysql | postgre | sqlite | and more which supported PDO (https://www.php.net/manual/en/pdo.drivers.php)
-$main_host = "localhost";           # databse: host server 
-$main_user = "root";                # databse: username
-$main_password = "root";            # databse: password
-$main_db = "museum";                # databse: database name
-
-$dsn = match($main_dsn) {
-    'mysql' => "mysql:host=$main_host;dbname=$main_db;charset=utf8mb4",
-    'pgsql' => "pgsql:host=$main_host;dbname=$main_db",
-    'sqlite' => "sqlite:/path/to/your/database.db",
-    'sqlsrv' => "sqlsrv:Server=$main_host;Database=$main_db",
-    'oci' => "oci:dbname=//$main_host:1521/$main_db",
-    default => throw new Exception("Unsupported database driver: $main_dsn")
-
-    # this is not the full list. add to it (pull request to https://github.com/bulatik205/museum/)
-};
-
-$pdo = new PDO($dsn, $main_user, $main_password, [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_EMULATE_PREPARES => true,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-]);
-?>
+return [
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'user' => 'root',
+    'password' => 'root',
+    'dbname' => 'museum'
+];
