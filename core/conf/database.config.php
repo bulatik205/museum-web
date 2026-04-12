aa75a196d747f5c24fed60935e2a822f6fce99d3
Булат Муллагалиев <bulatmullagal@gmail.com>
2026-04-12 11:08:46 +0400

    update: filled the fields for connect to db

--- core/conf/database.config.php
+++ core/conf/database.config.php
@@ -1,9 +1,9 @@
 <?php
 $main_dsn = "mysql";                # databse: mysql | postgre | sqlite | and more which supported PDO (https://www.php.net/manual/en/pdo.drivers.php)
 $main_host = "localhost";           # databse: host server 
-$main_user = "";                    # databse: username
-$main_password = "";                # databse: password
-$main_db = "";                      # databse: database name
+$main_user = "root";                # databse: username
+$main_password = "root";            # databse: password
+$main_db = "museum";                # databse: database name
 
 $dsn = match($main_dsn) {
     'mysql' => "mysql:host=$main_host;dbname=$main_db;charset=utf8mb4",
