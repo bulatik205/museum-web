e5f4cf9ee7aa9beb69aa7e21590b8023abdc29a4
Булат Муллагалиев <bulatmullagal@gmail.com>
2026-04-15 19:59:18 +0400

    HOTFIX: PDO::ATTR_EMULATE_PREPARES => true <---- crit. I edit to 'false'

--- src/App/Database/Database.php
+++ src/App/Database/Database.php
@@ -42,7 +42,7 @@ class Database
         
         return new PDO($dsn, $user, $password, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_EMULATE_PREPARES => true,
+            PDO::ATTR_EMULATE_PREPARES => false,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]);
     }
